750c3191ccdbc1d6b140603d0304fc51f0a82033
Kamil Grabek <kamil.grabek@allegro.pl>
2018-06-23 16:08:11 +0200

    Dodanie zależności dev jako normlanych, pełne budowanie paczki przy starcie

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const simpleGit = require('simple-git/promise');
+const spawn = require('child_process').spawn;
 
 const { HOST, HTML_PATHS, ROUTES, PORT, PUBLIC_PATH } = require('./config');
 
@@ -13,9 +15,35 @@ if (process.env.NODE_ENV === 'development') {
 
 // set static folder
 app.use(express.static(PUBLIC_PATH));
+let previousBranch = null;
 
-app.get(ROUTES.ROOT, (req, res) => {
-  res.sendFile(HTML_PATHS.APP);
+app.get(ROUTES.ROOT, async (req, res, next) => {
+  console.log(['previousBranch'], previousBranch);
+  console.log(['req.query.branch'], req.query.branch);
+  previousBranch = req.query.branch;
+  if (req.query.branch) {
+    await simpleGit().checkout(req.query.branch);
+    // Notice how your arguments are in an array of strings
+    const child = spawn('npm', [
+      'run',
+      'build:heroku',
+    ]);
+
+    child.stdout.on('data', function (data) {
+      process.stdout.write(data);
+    });
+
+    child.stderr.on('data', function (data) {
+      process.stdout.write(data);
+    });
+
+    child.on('exit', function (data) {
+      process.stdout.write('I\'m done!');
+      res.redirect('/');
+    });
+  } else {
+    res.sendFile(HTML_PATHS.APP);
+  }
 });
 
 // set Port
