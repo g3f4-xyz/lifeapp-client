403e7192be45b7dbdf1640f0f73ac38af6124d38
Kamil Grabek <kamil.grabek@allegro.pl>
2018-07-03 22:07:21 +0200

    [#9] Wyniesienie editHandlera do osobnego pliku w nowym folderze handlers

---
+++ src/handlers/editHandler.js
@@ -0,0 +1,134 @@
+import { FIELD_FORMATS } from '../constans';
+
+export default (data, update, { onSave }) => {
+  const mapFields = field => {
+    const {
+      fieldId,
+      format,
+      type,
+      label,
+      order,
+      meta: {
+        defaultValue,
+        options,
+        optionsSet,
+        parentId,
+      },
+      value: {
+        bool,
+        customValueOptionValue,
+        id,
+        ids,
+        number,
+        text,
+      }
+    } = field;
+    const commonProps = {
+      format,
+      id: fieldId,
+      label,
+      order,
+    };
+    const getField = id => data.fields.find(({ fieldId }) => fieldId === id);
+
+    if (fieldId === 'CYCLE') {
+      return {
+        options,
+        value: id,
+        onChange: ({ target: { value }}) => {
+          console.log(['editHandler.CYCLE.onChange'], fieldId, value);
+          update([fieldId, 'WHEN'], [
+            {
+              id: value,
+            },
+            {
+              ids: [],
+            },
+          ]);
+        },
+        ...commonProps,
+      }
+    }
+    else if (format === FIELD_FORMATS.BOOL) {
+      return {
+        checked: bool,
+        value: fieldId,
+        onChange: ({ target: { value }}, alternativeValue) => {
+          console.log(['editHandler.BOOL.onChange'], fieldId, value, alternativeValue);
+          update(fieldId, {
+            bool: alternativeValue,
+          });
+        },
+        ...commonProps,
+      }
+    }
+    else if (format === FIELD_FORMATS.CHOICE) {
+      return {
+        options,
+        value: id,
+        onChange: ({ target: { value }}) => {
+          console.log(['editHandler.CHOICE.onChange'], fieldId, value);
+          update(fieldId, {
+            id: value,
+          });
+        },
+        ...commonProps,
+      }
+    }
+    else if (format === FIELD_FORMATS.MULTIPLE_CHOICE_WITH_PARENT) {
+      const { value: { id: parentValue }, label: parentLabel } = getField(parentId);
+
+      return {
+        ids,
+        optionsSet,
+        defaultValue,
+        parentLabel,
+        parentValue,
+        customValueOptionValue,
+        onChange: ({ target: { value }}, { isCustomOptionValueUpdate = false } = {}) => {
+          console.log(['editHandler.MULTIPLE_CHOICE_WITH_PARENT.onChange'], { ids, fieldId, value, isCustomOptionValueUpdate });
+          update(fieldId, {
+            customValueOptionValue: isCustomOptionValueUpdate ? value : customValueOptionValue,
+            ids: isCustomOptionValueUpdate ? ids : value,
+            parentValue,
+          });
+        },
+        ...commonProps,
+      }
+    }
+    else if (format === FIELD_FORMATS.NUMBER) {
+      return {
+        value: number,
+        onChange: ({ target: { value }}) => {
+          console.log(['editHandler.NUMBER.onChange'], fieldId, value);
+          update(fieldId, {
+            number: value,
+          });
+        },
+        ...commonProps,
+      }
+    }
+    else if (format === FIELD_FORMATS.TEXT) {
+      return {
+        type,
+        value: text,
+        onChange: ({ target: { value }}) => {
+          console.log(['editHandler.TEXT.onChange'], fieldId, value);
+          update(fieldId, {
+            text: value,
+          });
+        },
+        ...commonProps,
+
+      }
+    }
+
+    return commonProps;
+  };
+
+  return {
+    fields: data.fields.map(mapFields),
+    taskType: data.taskType,
+    onSave,
+  }
+};
--- src/modules/Task/TaskFragment.jsx
+++ src/modules/Task/TaskFragment.jsx
@@ -5,7 +5,7 @@ import update from 'immutability-helper';
 import TaskDetails from './TaskDetails';
 import TaskEdit from './TaskEdit';
 import saveTaskMutation from '../../mutations/saveTask';
-import { FIELD_FORMATS } from '../../constans';
+import editHandler from '../../handlers/editHandler';
 
 const valueParser= field => {
   if (field.type === 'datetime-local') {
@@ -82,145 +82,12 @@ class TaskFragment extends React.Component {
     }));
   };
 
-  editHandler(data, update, { onSave }) {
-    const mapFields = field => {
-      const {
-        fieldId,
-        format,
-        type,
-        label,
-        order,
-        meta: {
-          defaultValue,
-          options,
-          optionsSet,
-          parentId,
-        },
-        value: {
-          bool,
-          customValueOptionValue,
-          id,
-          ids,
-          number,
-          text,
-        }
-      } = field;
-      const commonProps = {
-        format,
-        id: fieldId,
-        label,
-        order,
-      };
-      const getField = id => data.fields.find(({ fieldId }) => fieldId === id);
-
-      if (fieldId === 'CYCLE') {
-        return {
-          options,
-          value: id,
-          onChange: ({ target: { value }}) => {
-            console.log(['editHandler.CYCLE.onChange'], fieldId, value);
-            update([fieldId, 'WHEN'], [
-              {
-                id: value,
-              },
-              {
-                ids: [],
-              },
-            ]);
-          },
-          ...commonProps,
-        }
-      }
-      else if (format === FIELD_FORMATS.BOOL) {
-        return {
-          checked: bool,
-          value: fieldId,
-          onChange: ({ target: { value }}, alternativeValue) => {
-            console.log(['editHandler.BOOL.onChange'], fieldId, value, alternativeValue);
-            update(fieldId, {
-              bool: alternativeValue,
-            });
-          },
-          ...commonProps,
-        }
-      }
-      else if (format === FIELD_FORMATS.CHOICE) {
-        return {
-          options,
-          value: id,
-          onChange: ({ target: { value }}) => {
-            console.log(['editHandler.CHOICE.onChange'], fieldId, value);
-            update(fieldId, {
-              id: value,
-            });
-          },
-          ...commonProps,
-        }
-      }
-      else if (format === FIELD_FORMATS.MULTIPLE_CHOICE_WITH_PARENT) {
-        const { value: { id: parentValue }, label: parentLabel } = getField(parentId);
-
-        return {
-          ids,
-          optionsSet,
-          defaultValue,
-          parentLabel,
-          parentValue,
-          customValueOptionValue,
-          onChange: ({ target: { value }}, { isCustomOptionValueUpdate = false } = {}) => {
-            console.log(['editHandler.MULTIPLE_CHOICE_WITH_PARENT.onChange'], { ids, fieldId, value, isCustomOptionValueUpdate });
-            update(fieldId, {
-              customValueOptionValue: isCustomOptionValueUpdate ? value : customValueOptionValue,
-              ids: isCustomOptionValueUpdate ? ids : value,
-              parentValue,
-            });
-          },
-          ...commonProps,
-        }
-      }
-      else if (format === FIELD_FORMATS.NUMBER) {
-        return {
-          value: number,
-          onChange: ({ target: { value }}) => {
-            console.log(['editHandler.NUMBER.onChange'], fieldId, value);
-            update(fieldId, {
-              number: value,
-            });
-          },
-          ...commonProps,
-        }
-      }
-      else if (format === FIELD_FORMATS.TEXT) {
-        return {
-          type,
-          value: text,
-          onChange: ({ target: { value }}) => {
-            console.log(['editHandler.TEXT.onChange'], fieldId, value);
-            update(fieldId, {
-              text: value,
-            });
-          },
-          ...commonProps,
-
-        }
-      }
-
-      return commonProps;
-    };
-
-    return {
-      fields: data.fields.map(mapFields),
-      taskType: data.taskType,
-      onSave,
-    }
-  }
-
   render() {
     console.log(['TaskFragment:render'], this.props, this.state);
     const { editMode } = this.props;
 
     return editMode ? (
-      <TaskEdit {...this.editHandler(this.state, this.updateFieldsValue, { onSave: this.onSave })} />
+      <TaskEdit {...editHandler(this.state, this.updateFieldsValue, { onSave: this.onSave })} />
     )  : (
       <TaskDetails data={this.state} />
     );
