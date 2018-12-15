d77872f0f811a24aa7cec1e1a8bb8d9d325fc6f2
Kamil Grabek <kamil.grabek@allegro.pl>
2018-12-15 14:48:59 +0100

    [FIX] eslint fixes

--- src/registerPushWorker.js
+++ src/registerPushWorker.js
@@ -49,7 +49,7 @@ async function send(register) {
 function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
+    .replace(/\-/g, '+')// eslint-disable-line no-useless-escape
     .replace(/_/g, '/');
 
   const rawData = window.atob(base64);
