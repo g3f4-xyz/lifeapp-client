ca15024ed8a7aba61884ba574e1c6e2e0ac47cad
Kamil Grabek <kamil.grabek@allegro.pl>
2019-01-27 22:31:12 +0100

    refaktor parentId -> parentID

--- src/components/modules/Task/TaskFragment.jsx
+++ src/components/modules/Task/TaskFragment.jsx
@@ -133,7 +133,7 @@ export default createFragmentContainer(
               }
               parentValue
             }
-            parentId
+            parentID
           }
           ... on TextMetaType {
             required
