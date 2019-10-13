2fb1e2536a7af97b6259f83bb89654f45b318bad
Kamil Grabek <kamil.grabek@allegro.pl>
2019-10-13 15:52:39 +0200

    responsive grid jako FC

--- src/components/containers/responsive-grid/ResponsiveGrid.tsx
+++ src/components/containers/responsive-grid/ResponsiveGrid.tsx
@@ -1,5 +1,5 @@
-import React, { Children } from 'react';
-import { WidthProvider, Responsive, Layout, Layouts } from 'react-grid-layout';
+import React, { Children, FC, useCallback, useState } from 'react';
+import { Layout, Layouts, Responsive, WidthProvider } from 'react-grid-layout';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
@@ -28,48 +28,45 @@ function saveToLS(key: string, value: Layouts) {
   }
 }
 
-export default class ResponsiveGrid extends React.PureComponent {
-  state = {
-    layouts: JSON.parse(JSON.stringify(getFromLS('layouts') || {})),
-  };
+const ResponsiveGrid: FC = props => {
+  const [layouts, setLayouts] = useState(
+    JSON.parse(JSON.stringify(getFromLS('layouts') || {}))
+  );
 
-  static defaultProps = {
-    className: 'layout',
-    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
-    rowHeight: 30,
-  };
+  const resetLayout = useCallback(() => {
+    setLayouts({});
+  }, [setLayouts]);
 
-  resetLayout = () => {
-    this.setState({ layouts: {} });
-  };
+  const onLayoutChange = useCallback(
+    (_currentLayout: Layout[], allLayouts: Layouts) => {
+      saveToLS('layouts', allLayouts);
+      setLayouts(allLayouts);
+    },
+    [saveToLS, setLayouts],
+  );
 
-  onLayoutChange = (_currentLayout: Layout[], allLayouts: Layouts) => {
-    saveToLS('layouts', allLayouts);
-    this.setState({ layouts: allLayouts });
-  };
+  return (
+    <div>
+      <button onClick={resetLayout}>Reset Layout</button>
+      <ResponsiveReactGridLayout
+        className="layout"
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        rowHeight={50}
+        layouts={layouts}
+        onLayoutChange={onLayoutChange}
+      >
+        {Children.map(props.children, (node, index) => (
+          <div
+            key={index}
+            style={{ border: 'solid', overflow: 'scroll' }}
+            data-grid={{ w: 2, h: 3, x: index * 2, y: 0, minW: 2, minH: 3 }}
+          >
+            {node}
+          </div>
+        ))}
+      </ResponsiveReactGridLayout>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.resetLayout}>Reset Layout</button>
-        <ResponsiveReactGridLayout
-          className="layout"
-          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-          rowHeight={50}
-          layouts={this.state.layouts}
-          onLayoutChange={this.onLayoutChange}
-        >
-          {Children.map(this.props.children, (node, index) => (
-            <div
-              key={index}
-              style={{ border: 'solid', overflow: 'scroll' }}
-              data-grid={{ w: 2, h: 3, x: index * 2, y: 0, minW: 2, minH: 3 }}
-            >
-              {node}
-            </div>
-          ))}
-        </ResponsiveReactGridLayout>
-      </div>
-    );
-  }
-}
+export default ResponsiveGrid;
