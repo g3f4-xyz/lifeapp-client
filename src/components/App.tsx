7b9d88af58bf5710d45e59a0fa8505dc4a826bfa
Kamil Grabek <kamil.grabek@allegro.pl>
2019-10-13 19:03:16 +0200

    przeniesienie inicjalizacji webpush

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,6 @@
-import React, { FC, useCallback, useLayoutEffect, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { MODULES_IDS } from '../constans';
-import registerUserSubscription from '../service-worker/registerUserSubscription';
-import assetsServiceWorker from '../service-worker/serviceWorkerManager';
 import AppContext, { TaskParams } from './AppContext';
 import ErrorBoundary from './containers/error-boundary/ErrorBoundary';
 import ResponsiveGrid, { ResponsiveGridItem } from './containers/responsive-grid/ResponsiveGrid';
@@ -11,18 +9,6 @@ import SettingsQuery from './modules/settings/SettingsQuery';
 import TaskList from './modules/task-list/TaskList';
 import TaskTypeList from './modules/task-type-list/TaskTypeList';
 import Task from './modules/task/Task';
-
-assetsServiceWorker.register({
-  onUpdate(registration) {
-    console.info(['assetsServiceWorker.register.onUpdate'], registration);
-  },
-  onActivated(registration) {
-    console.info(['assetsServiceWorker.register.onActivated'], registration);
-  },
-  onSuccess(registration) {
-    console.info(['assetsServiceWorker.register.onSuccess'], registration);
-  },
-});
 
 const App: FC = () => {
   const [openedTasksParams, setOpenedTasksParams] = useState<TaskParams[]>([]);
@@ -55,20 +41,6 @@ const App: FC = () => {
       setOpenedTasksParams(filtered);
     }
   };
-
-  useLayoutEffect(() => {
-    try {
-      if (Notification.permission === 'granted') {
-        setTimeout(async () => {
-          const registration = await navigator.serviceWorker.ready;
-          console.info(['navigator.serviceWorker.ready'], registration);
-          await registerUserSubscription(registration, { silent: true });
-        }, 3000);
-      }
-    } catch (e) {
-      console.error(`error trying to send user subscription | ${e}`);
-    }
-  });
 
   return (
     <ErrorBoundary>
